f7c0c5d673a4347819e7e4830349fee2b87f22dd
Adhithya_Laxman <adhithyarg26@gmail.com>
2024-05-27 14:11:33 +0530

    Source Code added

---
+++ src/main/java/g1/testSite/App.java
@@ -0,0 +1,16 @@
+package g1.testSite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
+
